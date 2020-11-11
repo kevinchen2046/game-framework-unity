1a77069058074305d3b234330855ab47108379b7
kevinchen2046 <kevin-chen@foxmail.com>
2020-11-11 15:03:00 +0800

    x

---
+++ Assets/Scenes/model/ModelUser.cs
@@ -0,0 +1,7 @@
+public class ModelUser : vitamin.ModelBase
+{
+    public string name = "我是ModelUser";
+    public ModelUser(){
+        
+    }
+}
--- Assets/Scenes/ui/MainView.cs
+++ Assets/Scenes/ui/MainView.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 public class MainView : vitamin.ViewFairy
 {
+    [vitamin.Model]
+    public ModelUser user;
     public FairyGUI.GComponent info;
-    
     public FairyGUI.GButton btnSetting;
     public MainView(string uiname, string packname, vitamin.UIType uitype):base(uiname,packname,uitype){}
     
@@ -11,5 +12,8 @@ public class MainView : vitamin.ViewFairy
         btnSetting.onClick.Add(()=>{
             vitamin.Logger.log("BtnSetting Click!!!");
         });
+        vitamin.Logger.log(user.name);
+
+        this.execCommand("user.rename","KevinChen");
     }
 }
